ef1b42a5abdc7f76220d6b1c1b38e43565fa3ce2
Fernando Rocha Silva <rocha@Fernandos-MBP-3.attlocal.net>
2016-10-02 01:24:46 -0500

    Adding enemy spawner script / instantiating first enemy into the game scene

---
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+		GameObject enemy = Instantiate (enemyPrefab, new Vector3 (0, 0, 0), Quaternion.identity) as GameObject;
+		//changing the current enemy's transform to the enemy formation transform
+		enemy.transform.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
