fee481b05d2ef7cc796d791ff9e622d0e0867f66
Fernando Rocha Silva <rocha@Fernandos-MBP-3.attlocal.net>
2016-10-01 23:55:50 -0500

    adding player controller script, player sprite, adjusting ship speed

---
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float touchPosition = Input.mousePosition.x;
+		if (touchPosition < Screen.width / 2) {
+			//move left
+			transform.position += new Vector3 (-speed, 0, 0);
+		} else {
+			//move right
+			transform.position += new Vector3 (+speed, 0, 0);
+		}
+	}
+}
