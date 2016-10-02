34df9dd16ba1e8ff68c5d4dcb84dc4326bcad9d1
Fernando Rocha Silva <rocha@Fernandos-MBP-3.attlocal.net>
2016-10-02 16:45:18 -0500

    Adding enemy formation for first level

---
+++ Laser Defender/Assets/Scripts/Position.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Position : MonoBehaviour {
+	
+	// Update is called once per frame
+	void OnDrawGizmos () {
+		Gizmos.DrawWireSphere (transform.position, 1);
+	}
+}
